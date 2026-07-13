321e2d3c10e3cc03bce5d920d25d88e33bfce874
calliduz <juztyned@gmail.com>
2026-07-13 15:57:45 +0800

    Added disable devtool

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import DisableDevtool from "disable-devtool";
 import App from "./App.tsx";
 import { ErrorBoundary } from "./ErrorBoundary.tsx";
 import "./index.css";
+
+// Prevent DevTools inspection
+DisableDevtool({
+  url: "about:blank",
+  disableMenu: true,
+  clearLog: true,
+  disableSelect: false,
+  disableCopy: false,
+  interval: 200,
+});
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
